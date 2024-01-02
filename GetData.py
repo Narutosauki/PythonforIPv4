e14e889e2095e05af829f70f36734ad780f9d8f4
Narutosauki <130073311+Narutosauki@users.noreply.github.com>
2024-01-02 13:53:45 +0800

    Add files via upload

--- GetData.py
+++ GetData.py
@@ -1,38 +1,45 @@
 import requests
-import re
-from math import log2
+import math
 
-def process_rir_data(url, country_code):
-    response = requests.get(url)
-    data = response.text
-    
-    ipv4_entries = re.findall(rf'{country_code}\|ipv4\|(.+?)\|(\d+?)\|', data)
-    ipv4_addresses = [f"{ip}/{32 - int(log2(int(size)))}" for ip, size in ipv4_entries]
-    
-    return ipv4_addresses
+apnic_url = "http://ftp.apnic.net/stats/apnic/delegated-apnic-latest"
+response_apnic = requests.get(apnic_url)
+with open("delegated-apnic-latest", "wb") as apnic_file:
+    apnic_file.write(response_apnic.content)
 
-urls = [
-    ("https://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest", "US"),
-    ("https://ftp.ripe.net/ripe/stats/delegated-ripencc-extended-latest", "EU"),
-    ("https://ftp.apnic.net/stats/apnic/delegated-apnic-extended-latest", "CN"),
-    ("https://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest", "LA"),
-    ("https://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-extended-latest", "AF")
+# 提取CN和非CN的IPv4信息
+with open("delegated-apnic-latest", "r") as apnic_file:
+    apnic_lines = apnic_file.readlines()
+
+cn_ipv4_list = [line.split('|') for line in apnic_lines if 'CN' in line and 'ipv4' in line]
+foreign_ipv4_list = [line.split('|') for line in apnic_lines if 'CN' not in line and 'ipv4' in line]
+
+cn_ipv4_info = [(ip_info[3], 32 - int(math.log2(int(ip_info[4])))) for ip_info in cn_ipv4_list]
+foreign_ipv4_info = [(ip_info[3], 32 - int(math.log2(int(ip_info[4])))) for ip_info in foreign_ipv4_list]
+
+with open("ipv4_china.txt", "w") as china_file:
+    for ip, prefix_length in cn_ipv4_info:
+        china_file.write(f"{ip}/{prefix_length}\n")
+
+# 从四个网站获取IPv4地址并添加到ipv4_foreign.txt中
+foreign_urls = [
+    "https://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest",
+    "https://ftp.ripe.net/ripe/stats/delegated-ripencc-extended-latest",
+    "https://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest",
+    "https://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-extended-latest"
 ]
 
-china_ipv4_addresses = []
-foreign_ipv4_addresses = []
-for url, country_code in urls:
-    ipv4_addresses = process_rir_data(url, country_code)
-    
-    if country_code == "CN":
-        china_ipv4_addresses.extend(ipv4_addresses)
-    else:
-        foreign_ipv4_addresses.extend(ipv4_addresses)
-
-# 保存中国IP
-with open("ipv4_china.txt", "w") as china_file:
-    china_file.write("\n".join(china_ipv4_addresses))
-
-# 保存其他国家IP
 with open("ipv4_foreign.txt", "w") as foreign_file:
-    foreign_file.write("\n".join(foreign_ipv4_addresses))
+    
+    for ip, prefix_length in foreign_ipv4_info:
+        foreign_file.write(f"{ip}/{prefix_length}\n")
+
+    
+    for url in foreign_urls:
+        response_foreign = requests.get(url)
+        foreign_lines = response_foreign.text.split('\n')
+        
+        foreign_ipv4_list = [line.split('|') for line in foreign_lines if 'ipv4' in line]
+        foreign_ipv4_info = [(ip_info[3], 32 - int(math.log2(int(ip_info[4])))) for ip_info in foreign_ipv4_list]
+
+        for ip, prefix_length in foreign_ipv4_info:
+            foreign_file.write(f"{ip}/{prefix_length}\n")
